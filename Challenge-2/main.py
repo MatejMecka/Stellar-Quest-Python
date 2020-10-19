2a8def705535c73e583c06dcf9f115a3f217fe75
matejmecka <matej.plavevski+github@gmail.com>
2020-10-19 21:22:45 +0200

    remove commented line

--- Challenge-2/main.py
+++ Challenge-2/main.py
@@ -6,7 +6,7 @@ import requests
 
 # 1. Load Keys
 server = Server("https://horizon-testnet.stellar.org")
-#stellar_quest_keypair = Keypair.from_secret("Shhhhh")
+stellar_quest_keypair = Keypair.from_secret("Shhhhh")
 quest_account_pub_key = stellar_quest_keypair.public_key
 quest_account_priv_key = stellar_quest_keypair.secret
 
