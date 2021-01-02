ddeaf6be0216aad4256e8670c6e3ba87c2f8d681
matejmecka <matej.plavevski+github@gmail.com>
2021-01-02 01:29:24 +0100

    Finish SQ01

--- Quest-1/Challenge-8/main.py
+++ Quest-1/Challenge-8/main.py
@@ -2,7 +2,7 @@
 Challenge 8: Path Payments
 """
 from stellar_sdk import Server, Keypair, TransactionBuilder, Network, Asset
-import requests
+import json
 
 # 1. Load Keys
 server = Server("https://horizon-testnet.stellar.org")
@@ -10,18 +10,17 @@ stellar_quest_keypair = Keypair.from_secret("Shhhhh")
 quest_account_pub_key = stellar_quest_keypair.public_key
 quest_account_priv_key = stellar_quest_keypair.secret
 
-# 2. Create Another account
-asseet_to_buy = Asset("SRT", "GCDNJUBQSX7AJWLJACMJ7I4BC3Z47BQUTMHEICZLE6MU4KQBRYG5JY6B")
+# 2. Create Asset Objects
+asset_to_buy = Asset("SRT", "GCDNJUBQSX7AJWLJACMJ7I4BC3Z47BQUTMHEICZLE6MU4KQBRYG5JY6B") 
+native_asset = Asset('XLM')
 
-# 3. Fund Another account using TestBot
-print("Funding Random Account...")
+# 3. Get Path Payment
 
-url = 'https://friendbot.stellar.org'
-response = requests.get(url, params={'addr': random_keypair.public_key})
-print(f"Friendbot responded with {response}")
+path_payments = Server.strict_receive_paths(server, source=[native_asset], destination_asset=asset_to_buy, destination_amount='100').call()
+path = [Asset(asset['destination_asset_code'], asset['destination_asset_issuer']) for asset in path_payments['_embedded']['records']]
+print(path)
 
-
-# 4. Use Stellar Quest account to fund a random one
+# 4. Use Stellar Quest account to send path payment
 print("Building Transaction...")
 
 base_fee = server.fetch_base_fee()
@@ -32,14 +31,18 @@ transaction = (
         source_account=stellar_account,
         network_passphrase=Network.TESTNET_NETWORK_PASSPHRASE,
         base_fee=base_fee,
-    ).append_path_payment_strict_receive_op(
-    destination="GBBM6BKZPEHWYO3E3YKREDPQXMS4VK35YLNU7NFBRI26RAN7GI5POFBB",
-    send_code="XLM", 
-    send_issuer=None, 
-    send_max="1000", 
-    dest_code="GBP",
-    dest_issuer="GASOCNHNNLYFNMDJYQ3XFMI7BYHIOCFW3GJEOWRPEGK2TDPGTG2E5EDW",
-    dest_amount="5.50", path=path) 
+    ).append_change_trust_op(
+        asset_code=asset_to_buy.code, asset_issuer=asset_to_buy.issuer
+    ).append_path_payment_strict_send_op(
+        destination=quest_account_pub_key,
+        send_code="XLM", 
+        send_issuer=None, 
+        send_amount="10",
+        dest_code=asset_to_buy.code,
+        dest_issuer=asset_to_buy.issuer,
+        dest_min="1", 
+        path=path
+    ) 
     ).build()
 
 print('Signing Transaction...')
